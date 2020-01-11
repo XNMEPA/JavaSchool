9a4735b1b5e8fdf54a250f172a2b426a32171700
XNMEPA <magistr00@gmail.com>
2020-01-11 09:42:44 +0300

    Version 2.0 - Версия с демонстрацией.

--- JS Tasks/src/ru/malychev/jstasks/Main.java
+++ JS Tasks/src/ru/malychev/jstasks/Main.java
@@ -2,6 +2,55 @@ package ru.malychev.jstasks;
 
 public class Main {
     public static void main(String[] args) {
+        final boolean MAN = true;
+        final boolean WOMAN = false;
 
+        Person vasiliy = new Person(MAN, "Василий");
+        Person vasilisa = new Person (WOMAN, "Василиса");
+        Person petr = new Person(MAN, "Пётр");
+        Person alena = new Person (WOMAN, "Алёна");
+        Person zakhar = new Person (MAN, "Захар");
+        Person zakharia = new Person (WOMAN, "Захария");
+        Person viktor = new Person (MAN, "Виктор");
+        Person viktoria = new Person (WOMAN, "Виктория");
+
+// Василия женим на Петре.
+        System.out.println("\n======================================================================");
+        vasiliy.marry(petr);
+
+// Алёну выдаем замуж за Викторию.
+        System.out.println("\n======================================================================");
+        alena.marry(viktoria);
+
+// Василий хочет жениться на себе.
+        System.out.println("\n======================================================================");
+        vasiliy.marry(vasiliy);
+
+// Василия женим на Василисе.
+        System.out.println("\n======================================================================");
+        vasiliy.marry(vasilisa);
+// Василия еще раз женим на Василисе.
+        System.out.println("\n======================================================================");
+        vasiliy.marry(vasilisa);
+
+// Петра женим на Алёне.
+        System.out.println("\n======================================================================");
+        petr.marry(alena);
+
+// Захара женим на Захарии.
+        System.out.println("\n======================================================================");
+        zakhar.marry(zakharia);
+
+// Викторию выдаем замуж за Виктора.
+        System.out.println("\n======================================================================");
+        viktoria.marry(viktor);
+
+// Василия женим на Виктории.
+        System.out.println("\n======================================================================");
+        vasiliy.marry(viktoria);
+
+// Алена выходит замуж за Виктора.
+        System.out.println("\n======================================================================");
+        alena.marry(viktor);
     }
 }
--- JS Tasks/src/ru/malychev/jstasks/Person.java
+++ JS Tasks/src/ru/malychev/jstasks/Person.java
@@ -8,37 +8,61 @@ public class Person {
     public Person(boolean man, String name) {
         this.man = man;
         this.name = name;
+        System.out.println((man ? "Создан мужчина - " : "Создана женщина - ") + this.name + ".");
     }
 
     public boolean getMan() {
         return man;
     }
 
+    public String getName() {
+        return name;
+    }
+
+
     public void setSpouse(Person person) {
         spouse = person;
+        if (person != null)
+            System.out.println(name + (man ? " женился на " : " вышла замуж за ") + person.getName() + ".");
     }
 
     public Person getSpouse() {
-        return spouse;
+            return spouse;
     }
 
     public boolean marry(Person person) {
-        if (this.man == person.getMan() || this == person.spouse) return false;
-        else {
+        System.out.println(name + " и " + person.getName() + " должны пожениться.");
+        if (this == person) {
+            System.out.println("Оригинально конечно! Но так не получится!");
+            return false;
+        } else if (this.man == person.getMan()) {
+            System.out.println("Мы не приветствуем однополые браки!");
+            return false;
+        } else if (this == person.getSpouse()) {
+            System.out.println("Они же уже женаты!");
+            return false;
+        } else {
             this.divorce();
-            this.spouse.divorce();
             person.divorce();
-            person.spouse.divorce();
             this.setSpouse(person);
             person.setSpouse(this);
+            System.out.println(name + " и " + person.getName() + " со счастливым вас браком!");
             return true;
         }
     }
 
     public boolean divorce() {
-        if (spouse == null) return false;
-        else {
+        if (spouse == null) {
+            System.out.println(name + (man ? " холост, не надо разводить." : " одинока, не надо разводить."));
+            return false;
+
+        } else {
+            System.out.println(name + " и " + spouse.getName() + " должны разойтись.");
+            System.out.println(name + (man ? " разведен." : " разведена.") + " Счастливого пути!");
+            System.out.println(spouse.getName() + (spouse.getMan() ? " разведен." : " разведена.") + " Счастливого пути!");
+            spouse.setSpouse(null);
             spouse = null;
+
             return true;
         }
     }
