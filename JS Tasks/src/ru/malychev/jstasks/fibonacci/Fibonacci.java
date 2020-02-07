32c94fbc587bfcb21f5111f29ef41a22d8c7a570
XNMEPA <magistr00@mail.com>
2020-02-07 16:34:58 +0300

    Решение задачи по генерации ряда чисел Фибоначчи.

--- JS Tasks/src/ru/malychev/jstasks/fibonacci/Fibonacci.java
+++ JS Tasks/src/ru/malychev/jstasks/fibonacci/Fibonacci.java
@@ -1,33 +1,62 @@
 package ru.malychev.jstasks.fibonacci;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.BiConsumer;
-import java.util.function.BiFunction;
-import java.util.function.Consumer;
+import java.util.stream.Stream;
 
 public class Fibonacci {
+    static int maxNumber = 50;
+    static List<Long> list = new ArrayList<>(maxNumber);
+
+    static BiConsumer<Integer, Long> printMember = (index, memberFibonacci) -> System.out.println("Fib[ " + index + " ] = " + memberFibonacci);
+
+    public static void fibonacci(int index, long a, long b, BiConsumer<Integer, Long> printMember) {
+        if (index > maxNumber) return;
+        printMember.accept(index, b);
+        fibonacci(++index, b, a + b, printMember);
+    }
+
+    public static void fibonacci(long a, BiConsumer<Integer, Long> show) {
+        long c, b = a;
+        for (int i = 1; i <= maxNumber ; i++) {
+            show.accept(i, a);
+            c = a; a = b; b +=c;
+        }
+    }
+
+    public static long fibonacci (long a, long b, int i) {
+        if (i < maxNumber)
+            list.add(fibonacci(b, a+b, ++i));
+        return a;
+    }
 
     public static void main(String[] args) {
 
-//        for (long a = 1, b = 1, c, i = 0; i < 50; i++) {
-//            System.out.println("a = " + a);
-//            c = a; a = b ; b += c;
+/*        for (long a = 0, b = 1, c, i = 1; i <= maxNumber; i++) {
+            System.out.println("Fib[ " + i + " ] = " + b);
+            c = a; a = b; b += c;
+        }
+*/
 
-//        fibonacci(50, 1, (i, a) -> {
-//            System.out.println("Fib[ " + i + " ] = " + a);
-//         });
-        fibonacci(0, 1, 1);
-    }
-//    public static void fibonacci(int n, long a, BiConsumer<Integer, Long> show) {
-//        long c, b = a;
-//        for (int i = 0; i < n ; i++) {
-//            show.accept(i, a);
-//            c = a; a = b; b +=c;
-//        }
-//    }
+//        fibonacci(1, 0, 1, printMember);
 
-    public static void fibonacci (long a, long b, int i) {
-        if (i == 50) return;
-        System.out.println("Fib[ " + i + " ] = " + b);
-        fibonacci(b, a+b, ++i);
+/*        fibonacci(0, 1, 1);
+        list.stream()
+            .sorted()
+            .forEach(System.out::println);
+*/
+/*        fibonacci(50, 1, (i, a) -> {
+            System.out.println("Fib[ " + i + " ] = " + a);
+         });
+*/
+        Stream.iterate(
+                new Long[] { 0L, 1L, 1L },
+                f -> new Long[] { f[1], f[0] + f[1], ++f[2]})
+                .map(f -> "Fib[" + f[2] + "] = " + f[1])
+                .limit(maxNumber)
+                .forEach(System.out::println);
+
     }
+
 }
