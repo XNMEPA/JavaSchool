7f8a831e96d75d0060df64912fdced2c13474546
XNMEPA <magistr00@gmail.com>
2020-02-17 11:02:27 +0300

    Решение задачи по пулу потоков исполнения.

--- JS Tasks/src/ru/malychev/jstasks/threadpool/ThreadPool.java
+++ JS Tasks/src/ru/malychev/jstasks/threadpool/ThreadPool.java
@@ -1,34 +1,43 @@
 package ru.malychev.jstasks.threadpool;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.*;
+import java.util.stream.Stream;
 
 public class ThreadPool {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException, ExecutionException {
 		int maxTasks = 15;
-		ExecutorService threadPool = Executors.newCachedThreadPool();
 
-		for (int i = 0; i < maxTasks; i++) {
-			final int count = i;
-			threadPool.execute (() -> {
-				System.out.println("Задача №: " + count + " запущена в потоке " + Thread.currentThread().getName());
-				try {
-					TimeUnit.SECONDS.sleep((long) (Math.random() * 10));
-				} catch (InterruptedException e) {
-					System.out.println("Работа задачи №: " + count + " была прервана.");
-				}
-				System.out.println("Я задача №: " + count + ". Продолжаю работать.");
-				try {
-					TimeUnit.SECONDS.sleep((long) (Math.random() * 10));
-				} catch (InterruptedException e) {
-					System.out.println("Работа задачи №: " + count + " была прервана.");
-				}
-				System.out.println("Задача №: " + count + " успешно завершена.");
-			});
+		List<Callable<String>> callableList = new ArrayList<>();
+		ExecutorService threadPool = Executors.newFixedThreadPool(3);
 
-		}
+			Stream.iterate(0,
+				count -> ++count)
+				.map(count -> {
+					return (Callable<String>) () -> {
+						String taskName = "Задача №: " + count;
+						try {
+							Thread.sleep((long) (Math.random() * 5000));
+							System.out.println(taskName + " запущена в потоке " + Thread.currentThread().getName());
+							Thread.sleep((long) (Math.random() * 10000));
+							System.out.println("Я " + taskName + ". Продолжаю работать в потоке " + Thread.currentThread().getName());
+							Thread.sleep((long) (Math.random() * 5000));
+						} catch (InterruptedException e) {
+							System.out.println(taskName + " была прервана.");
+						}
+						System.out.println(taskName + " успешно завершена.");
+						return taskName + " успешно завершена.";
+					};
+				})
+				.limit(maxTasks)
+				.forEach(callableList::add);
+
+		List<Future<String>> resultsEndOfTask = threadPool.invokeAll(callableList);
 		threadPool.shutdown();
+		System.out.println("=====================================================");
+		for (Future<String> resultTask : resultsEndOfTask)
+			System.out.println(resultTask.get());
 	}
 }
